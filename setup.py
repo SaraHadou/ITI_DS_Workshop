c9008a718aea4a0c31aa894bc6e8995a1ee5715c
Sara Elaraby <saraelaraby@Sarahs-MacBook-Air.local>
2021-09-22 01:01:51 +0200

    first directory

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Deena gergis workshop',
+    author='Sara Elaraby',
+    license='',
+)
